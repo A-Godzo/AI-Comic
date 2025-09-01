830a5c6300d9a1d16eef43ee1193f32531a70e68
A-Godzo <godzoandon@gmail.com>
2025-09-01 20:52:54 +0200

    added some stuff

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/generate-comic', async (req, res) => {
       const resp = await client.images.generate({
         model: "gpt-image-1",   // DALL·E 3
         prompt,
-        size: "1024x1024",
+        size: "512x512",
       });
 
       const b64 = resp.data?.[0]?.b64_json;
